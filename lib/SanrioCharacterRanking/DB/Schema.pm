513fc7702d42a5e304a970cc6561a66e088c6cc4
mono <mono@monoweb.info>
2015-05-16 17:54:10 +0900

    Update SanrioCharacterRanking::DB::Schema

--- lib/SanrioCharacterRanking/DB/Schema.pm
+++ lib/SanrioCharacterRanking/DB/Schema.pm
@@ -8,9 +8,20 @@ use Teng::Schema::Declare;
 base_row_class 'SanrioCharacterRanking::DB::Row';
 
 table {
-    name 'member';
+    name 'statuses';
     pk 'id';
-    columns qw(id name);
+    columns qw(time source);
+};
+
+table {
+    name 'characters';
+    pk 'id';
+    columns qw(name);
+};
+
+table {
+    name 'votes';
+    pk qw(character_id status_id);
 };
 
 1;
