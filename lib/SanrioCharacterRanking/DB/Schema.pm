e1da81965be95f4e65e0fa8b32e1bf4b7f8cabee
mono <mono@monoweb.info>
2015-05-17 09:14:22 +0900

    Fix schema

--- lib/SanrioCharacterRanking/DB/Schema.pm
+++ lib/SanrioCharacterRanking/DB/Schema.pm
@@ -10,18 +10,19 @@ base_row_class 'SanrioCharacterRanking::DB::Row';
 table {
     name 'statuses';
     pk 'id';
-    columns qw(created_at source);
+    columns qw(id created_at source);
 };
 
 table {
     name 'characters';
     pk 'id';
-    columns qw(name);
+    columns qw(id name);
 };
 
 table {
     name 'votes';
     pk qw(character_id status_id);
+    columns qw(character_id status_id);
 };
 
 1;
