1052241b86b9b664d3f94330326f8621f8b86f4d
mono <mono@monoweb.info>
2015-05-27 23:49:12 +0900

    Update SQL for generating ranking

--- lib/SanrioCharacterRanking/M/Vote.pm
+++ lib/SanrioCharacterRanking/M/Vote.pm
@@ -10,8 +10,22 @@ use Amon2::Declare;
 
 sub ranking {
     my ($class) = @_;
-    c()->db->search_by_sql(
-        q{SELECT characters.name AS character_name, COUNT(statuses.id) AS votes FROM statuses JOIN votes ON votes.status_id = statuses.id JOIN characters ON characters.id = votes.character_id GROUP BY characters.name ORDER BY votes desc;});
+    c()->db->search_by_sql('
+        SELECT
+            ranking.character_id AS character_id,
+            characters.name AS character_name,
+            ranking.votes AS votes
+        FROM (
+            SELECT
+                votes.character_id AS character_id,
+                COUNT(statuses.id) AS votes
+            FROM statuses
+            JOIN votes ON votes.status_id = statuses.id
+            GROUP BY character_id
+        ) ranking
+        JOIN characters ON ranking.character_id = characters.id
+        ORDER BY votes DESC, character_id ASC
+    ');
 }
 
 1;
